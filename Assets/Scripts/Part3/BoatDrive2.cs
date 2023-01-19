80f5c6171dc6a94532858735758e10f46e359e21
dabere_abasse <106044798+abasse-dabere@users.noreply.github.com>
2023-01-19 19:16:04 +0100

    Partie 3 afficher Bravo et aller au niveau superieur

--- Assets/Scripts/Part3/BoatDrive2.cs
+++ Assets/Scripts/Part3/BoatDrive2.cs
@@ -5,11 +5,17 @@ using System.IO;
 
 public class BoatDrive2 : MonoBehaviour
 {
-    public float speed = 15.0f;
-    public float rotationSpeed = 90.0f;
-    public float maxRotationSpeed = 20.0f;
+    public float speed = 10.0f;
+    public float rotationSpeed = 500.0f;
+    public float maxRotationSpeed = 1.0f;
+
+    public Transform arrivalPoint;
+    public float distanceThreshold = 1.0f;
+    private bool reached = false;
 
     private Rigidbody rb;
+
+    public GameObject cameraObject;
     //public float maxAngleDeg;
     //float maxAngle;
 
@@ -42,5 +48,20 @@ public class BoatDrive2 : MonoBehaviour
             transform.rotation = newRotation;
         }
         transform.position += moveDirection * speed * Time.deltaTime;
+
+        // Check distance to arrival point
+        float distance = Vector3.Distance(transform.position, arrivalPoint.position);
+        if (distance <= distanceThreshold && !reached)
+        {
+            reached = true;
+
+            //felicitations
+            Debug.Log("Bravoo!");
+
+            //Move camera
+            cameraObject.SendMessage("goToNext");
+            
+        }
     }
+
 }
