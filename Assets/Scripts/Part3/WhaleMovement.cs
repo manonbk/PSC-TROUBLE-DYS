15ae5941fee6164aad9ea4af6d33090838a7edef
clementrouxx <114852217+clementrouxx@users.noreply.github.com>
2023-02-20 15:34:55 +0100

    Bezier Deux points

--- Assets/Scripts/Part3/WhaleMovement.cs
+++ Assets/Scripts/Part3/WhaleMovement.cs
@@ -5,6 +5,7 @@ public class WhaleMovement : MonoBehaviour
 {
     public Transform[] checkpoints;
     public Transform[] bezierPoints;
+    public Transform[] secondaryBezierPoints;
     public float amplitude = 0.8f;
     public float frequency = 1f;
     public float movementDuration = 2f;
@@ -22,10 +23,10 @@ public class WhaleMovement : MonoBehaviour
 
     public void GoToCheckpoint(int index, float duration, float delay)
     {
-        StartCoroutine(GoTo(checkpoints[index].position, duration,delay,bezierPoints[index-1].position));
+        StartCoroutine(GoTo(checkpoints[index].position, duration,delay,bezierPoints[index-1].position, secondaryBezierPoints[index - 1].position));
     }
 
-    private IEnumerator GoTo(Vector3 targetPos, float duration, float delay, Vector3 bezierPos)
+    private IEnumerator GoTo(Vector3 targetPos, float duration, float delay, Vector3 bezierPos, Vector3 secondaryBezierPos)
     {
         yield return new WaitForSeconds(delay);
 
@@ -35,9 +36,9 @@ public class WhaleMovement : MonoBehaviour
         while (progress <= 1)
         {
             float t = Mathf.SmoothStep(0, 1, progress);
-            transform.position = (1 - t)*(1-t) * startingPos + 2*t*(1-t)* bezierPos + t*t*targetPos;
+            transform.position = (1 - t)*(1-t)*(1-t) * startingPos + 3*t*(1-t)*(1-t)* bezierPos + 3*t*t*(1-t)*secondaryBezierPos + t*t*t*targetPos;
             progress += Time.deltaTime / duration;
-            transform.rotation = Quaternion.LookRotation(-(-2 * (1 - t) * startingPos + 2 * (1 - 2 * t) * bezierPos + 2 * t * targetPos));
+            transform.rotation = Quaternion.LookRotation(-(-3 * (1 - t)*(1-t) * startingPos + 3 * ((1-t) - 2*t)* (1 - t) * bezierPos + 3*t*(-t+2*(1-t))*secondaryBezierPos + 3*t*t* targetPos));
             yield return null;
         }
     }
