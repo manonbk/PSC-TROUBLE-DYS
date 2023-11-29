a4263ec014f563afecfdc0062877347c48bfed94
margotaubusson <margotaubusson@gmail.com>
2023-11-29 15:46:49 +0100

    Update movesystem

--- Assets/Scripts/Part2/MoveSystem.cs
+++ Assets/Scripts/Part2/MoveSystem.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveSystem : MonoBehaviour
+//ce script permet de bouger l'objet et de le bloquer sur la bonne forme
 {
-    public GameObject correctForm;
+    public GameObject correctForm; //cible
     private bool moving;
-
+    private bool finish; //est-ce vraiment la bonne forme ?
     private float startPosX;
     private float startPosY;
+
+    private Vector3 resetPosition; //position de départ si erreur 
 
 
     void Start()
     {
-        
+        resetPosition = this.transform.localPosition;
     }
 
     void Update()
     {
-        if (moving) {
-            Vector3 mousePos;
-            mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        if (finish == false) { //on ne peut plus bouger l'objet s'il est sur la bonne forme
+            if (moving) {
+                Vector3 mousePos;
+                mousePos = Input.mousePosition;
+                mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
-            transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, transform.localPosition.z);
+                transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, transform.localPosition.z);
+            }
         }
     }
     
@@ -42,6 +47,15 @@ public class MoveSystem : MonoBehaviour
 
     private void OnMouseUp() {
         moving = false;
+
+        if (Mathf.Abs(transform.localPosition.x - correctForm.transform.localPosition.x) <= 0.1f &&  //distance 0,5 à redéfinir = précision qu'on attend pour lacher
+            Mathf.Abs(transform.localPosition.y - correctForm.transform.localPosition.y) <= 0.1f) {
+                this.transform.position = new Vector3(correctForm.transform.position.x,correctForm.transform.position.y,correctForm.transform.position.z);
+                finish = true;
+            }
+        else {
+            this.transform.localPosition = new Vector3(resetPosition.x,resetPosition.y, resetPosition.z);
+        }
     }
 
 }
