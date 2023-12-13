b36eb83507edd1cdc3633ac920e40fadc7d6f410
margotaubusson <margotaubusson@gmail.com>
2023-12-13 15:05:33 +0100

    Update Scaling.cs

--- Assets/Scripts/Part2/Scaling.cs
+++ Assets/Scripts/Part2/Scaling.cs
@@ -18,8 +18,8 @@ public class Scaling : MonoBehaviour
     void Update()
     {
         if (Input.touchCount ==2 && (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(1).phase == TouchPhase.Moved)) {
-            Vector3 touch1 = Input.GetTouch(0).position;
-            Vector3 touch2 = Input.GetTouch(1).position;
+            Vector2 touch1 = Input.GetTouch(0).position;
+            Vector2 touch2 = Input.GetTouch(1).position;
 
             newDistance = (touch1 - touch2).sqrMagnitude;
             float changeInDistance = newDistance - initialDistance;
@@ -35,10 +35,17 @@ public class Scaling : MonoBehaviour
     private void OnMouseDown(){
         //verifie si 2 touches
         if (Input.touchCount == 2 && Input.GetTouch(1).phase == TouchPhase.Began) {
-            Vector3 touch1 = Input.GetTouch(0).position;
-            Vector3 touch2 = Input.GetTouch(1).position;
+            Vector2 touch1 = Input.GetTouch(0).position;
+            Vector2 touch2 = Input.GetTouch(1).position;
 
             initialDistance = (touch1 - touch2).sqrMagnitude;
+        }
+    }
+
+    private void OnMouseUp(){
+        //si moins de 2 touches, remet la distance initiale 
+        if (Input.touchCount<2){
+            initialDistance=0;
         }
     }
 }
