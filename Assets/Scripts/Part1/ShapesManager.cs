e8bb3649bc07bd14a841e4a66b430b0ffabd391c
manonbk <manon.ben-kiran@polytechnique.edu>
2024-01-10 16:25:23 +0100

    mise a jour du shape manager activité 1
    ca va etre plus simple pour gérer les formes et vérifier que c'est le bon niveau.

--- Assets/Scripts/Part1/ShapesManager.cs
+++ Assets/Scripts/Part1/ShapesManager.cs
@@ -9,9 +9,11 @@ public class ShapesManager : MonoBehaviour
     // Script qui gère les images : fonctions pour dessiner, 
 
     public string[] shapeNames;
-    int currentShapeIndex = -1;
+    public int[] shapeLevel;
+    public bool [] shapeAchieved;
+    int currentShapeIndex = 0;
     public DrawTemplate drawTemplate; // C'est un script
-    public Draw draw;
+    public Draw draw; // script aussi. Différence avec draw?
     public GameObject startButton;
     public GameObject endPanel;
 
@@ -30,7 +32,7 @@ public class ShapesManager : MonoBehaviour
     }
  
 
-    public void StartGame()
+    public void StartGame() // Quand estce qu'elle s'execute ?
     {
         draw.hasStarted = true;
         startButton.SetActive(false);
@@ -40,13 +42,7 @@ public class ShapesManager : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void SetShapeIndex(int i)
+    void SetShapeIndex(int i) // permet d'aller une forme à lautre
     {
         currentShapeIndex = i;
         string shapeName = shapeNames[currentShapeIndex];
@@ -54,14 +50,19 @@ public class ShapesManager : MonoBehaviour
         draw.SetShape(shapeName);
     }
 
-    public void NextShape()
+    public void NextShape() // on cherche la prochaine forme du niveau requis non déja faite
     {
-        if (currentShapeIndex == shapeNames.Length - 1)
+        int potentialIndex = currentShapeIndex;
+        bool found = false ; // si on a trouvé un bon indice
+        bool fini = false; //si on a tout testé on retourne au menu
+        while (!found && !fini)
         {
-            SceneManager.LoadScene(0);
+            potentialIndex= ((potentialIndex+1) % shapeNames.Length);
+            if (potentialIndex==currentShapeIndex) {fini=true;} // on a fait le tour
+            if (!shapeAchieved[potentialIndex]) {found=true;}
+
         }
-        else
-        SetShapeIndex(currentShapeIndex+1);
+        if (fini) {SceneManager.LoadScene(0);} else {SetShapeIndex(potentialIndex);}
     }
 
     public void PreviousShape()
@@ -81,6 +82,7 @@ public class ShapesManager : MonoBehaviour
     {
         if (draw.isDrawingFinished)
         {
+            shapeAchieved[currentShapeIndex]=true;
             NextShape();
         }
         else
