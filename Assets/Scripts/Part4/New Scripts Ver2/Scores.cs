4c818556657561acc517302090b8422c566604d2
nath2804 <152400542+nath2804@users.noreply.github.com>
2024-03-13 16:39:36 +0100

    Erreurs de compilation résolue

--- Assets/Scripts/Part4/New Scripts Ver2/Scores.cs
+++ Assets/Scripts/Part4/New Scripts Ver2/Scores.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using Unity.VisualScripting.FullSerializer;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SocialPlatforms.Impl;
--- Assets/Scripts/Part4/ScoreManager.cs
+++ Assets/Scripts/Part4/ScoreManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting.FullSerializer;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SocialPlatforms.Impl;
